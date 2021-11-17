a2158bc360b5019f85f512f88a8f53342ab3feb7
LeshaGamer <94561950+LeshaGamer@users.noreply.github.com>
2021-11-17 22:38:35 +0300

    Add files via upload

---
+++ L298N.h
@@ -0,0 +1,11 @@
+#define ena 2
+#define enb 3
+#define in1a A0
+#define in2a A1
+#define in1b A2
+#define in2b A3
+
+void L298Nleft(int speedRotateA, int speedRotate B);
+void L298Nright(int speedRotateA, int speedRotate B);
+void L298Nstop(int speedRotateA, int speedRotate B);
+void L298Nforw(int speedRotateA, int speedRotate B);
